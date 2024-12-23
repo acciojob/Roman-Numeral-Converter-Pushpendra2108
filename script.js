cf43562be858f216baf6b0b563d50227732eee46
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-23 13:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const romanSymbols = {
+        0:['M',1000], 
+        1:['CM',900],
+        2:['D', 500], 
+        3:['CD',400],
+        4:['C', 100], 
+        5:['XC',90],
+        6:['L', 50], 
+        7:['XL',40],
+        8:['X', 10], 
+        9:['IX',9],
+        10:['V', 5], 
+        11:['IV',4],
+        12:['I', 1]
     };
-	
-  let result = "";
 
-  // Loop through the obj array
-  for (const [symbol, value] of Object.values(obj)) {
-    while (num >= value) {
-      result += symbol; 
-      num -= value;     
+    let result = "";
+
+    // Loop through the romanSymbols array
+    for (const [symbol, value] of Object.values(romanSymbols)) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;     
+        }
     }
-  }
 
-  return result;
+    return result;
 }
 
 // console.log(convertToRoman(36));
 
-module.exports = convertToRoman
+module.exports = convertToRoman;
